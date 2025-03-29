2a0f36f4f5eeab5c6f8c1612fdcafc288f9f77c7
Hasanur Rahman <mdhasanurdev@gmail.com>
2025-03-29 17:02:51 +0600

    day 4 add popscope

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,23 +13,30 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/village_of_bangladesh.jpg',
-                width: 400,
-                height: 150,
-              ),
-              SizedBox(height: 10.0),
-              Text(title, style: TextStyle(fontSize: 20.0)),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child:  Text(details, style: TextStyle(fontSize: 14.0), textAlign: TextAlign.justify),
-              ),
-            ],
+      body: PopScope(
+        canPop: false,
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/village_of_bangladesh.jpg',
+                  width: 400,
+                  height: 150,
+                ),
+                SizedBox(height: 10.0),
+                Text(title, style: TextStyle(fontSize: 20.0)),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: Text(
+                    details,
+                    style: TextStyle(fontSize: 14.0),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
